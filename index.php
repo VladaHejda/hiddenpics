44460c971c0c26ddf3292f8b91464a5172c47a01
Vlada Hejda <hejdavladislav@gmail.com>
2018-02-13 23:22:02 +0100

    Mobile does not accept integrity :-\

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $horizontalMaxCount = findMaxCount($horizontalNumbers);
 $verticalMaxCount = findMaxCount($verticalNumbers);
 
 ?>
-<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 
 <script>
 	$(function () {
